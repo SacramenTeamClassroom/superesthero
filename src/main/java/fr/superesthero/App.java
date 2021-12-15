2fd49fc5de6c48ac06c693db2f51817f321cb206
Sacramentix <benji.u1225u@gmail.com>
2021-12-15 16:36:47 +0100

    move config into entry point

--- src/main/java/fr/superesthero/App.java
+++ src/main/java/fr/superesthero/App.java
@@ -2,17 +2,10 @@ package fr.superesthero;
 
 import static spark.Spark.port;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import org.hibernate.cfg.Configuration;
-import org.hibernate.service.ServiceRegistry;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 import fr.superesthero.controllers.CategoryController;
 import fr.superesthero.controllers.HeroController;
@@ -24,11 +17,19 @@ public class App {
     public static SessionFactory factory;
     
     public static void main(String[] args) {
+        
+        Configuration configuration = new Configuration()
+            .addAnnotatedClass(Hero.class)
+            .addAnnotatedClass(Category.class)
+            .setProperty("hibernate.connection.driver_class", "org.sqlite.JDBC")
+            .setProperty("hibernate.connection.url", "jdbc:sqlite:app.sqlite")
+            .setProperty("hibernate.dialect", "org.sqlite.hibernate.dialect.SQLiteDialect")
+            .setProperty("hibernate.show_sql", "true")
+            .setProperty("hibernate.hdm2ddl.auto", "update");
+            // .setProperty("transaction.factory_class","org.hibernate.transaction.JDBCTransactionFactory");
+            
 
-        factory = new Configuration().configure("/fr/superesthero/hibernate.cfg.xml").buildSessionFactory();
-        // var sources = new MetadataSources(registry);
-        // var metadata = sources.getMetadataBuilder().build();
-        // factory = metadata.getSessionFactoryBuilder().build();
+        factory = configuration.buildSessionFactory();
 
         start();
     }
@@ -39,8 +40,6 @@ public class App {
         System.out.println("Start on port " + port);
         CategoryController.init();
         HeroController.init();
-        Category.add("comic");
-        Hero.add("Clark", "superman", new ArrayList<Category>(List.of(Category.get(0))));
     }
 
 }
